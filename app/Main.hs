0f0dacdb575b27111a2be357b815b1f50d8e07d0
Jake Wood <jzwood14@yahoo.com>
2023-11-24 21:17:46 -0500

    wip

--- app/Main.hs
+++ app/Main.hs
@@ -10,9 +10,29 @@ import qualified Data.ByteString.Lazy.Char8 as BLC
 import Data.Maybe (fromMaybe)
 import Format
 import Handle
-import Network.Simple.TCP (HostPreference (..), recv, send, serve)
+--import Network.Simple.TCP (HostPreference (..), recv, send, serve)
 import Syntax
 import System.Environment (getArgs)
+
+import Control.Concurrent (ThreadId, forkIO)
+import Data.Function
+import Data.Functor
+import Network.Socket (
+    Family (..),
+    SockAddr (..),
+    Socket,
+    SocketOption (..),
+    SocketType (..),
+    PortNumber (..),
+    accept,
+    bind,
+    close,
+    defaultProtocol,
+    listen,
+    setSocketOption,
+    socket,
+ )
+import Network.Socket.ByteString (recv, send)
 
 did :: ByteString
 did = "id: 0"
@@ -26,35 +46,51 @@ datum = "data: hello world"
 sser :: ByteString
 sser = "200 OK\r\ncontent-type: text/event-stream\r\n\r\n"
 
+eachConn :: Env -> Socket -> IO ()
+eachConn env conn = do
+    req <- recv conn 1024
+    res <- handle env req
+    print req
+    _ <- send conn (toBs res)
+    close conn
+    --case runParser req of
+        --Right rast -> do
+            ----res <- Handler.handle rast
+            --send conn "200" -- res
+            ----_ <- forkIO $ handle conn
+            --return ()
+        --Left _ -> close conn
+
+--let isSse = B.empty == getHeader "text/event-stream" (headers' bsRes)
+--if isSse
+--then do
+    --print "SSE START\n\n"
+    --send serverSocket (toBs bsRes)
+    --print (toBs bsRes)
+    --forever $ do
+        ----forkIO $ handle conn store
+        --print "SSE\n\n"
+        --send serverSocket did
+        --send serverSocket ddata
+--else send serverSocket (toBs bsRes)
+
 main :: IO ()
 main = do
     argv <- getArgs
 
     BLC.putStrLn "Logs from your program will appear here"
 
-    let host = "127.0.0.1"
-        port = "4221"
+    let port = 4221 :: PortNumber
         env = case argv of
             ["--directory", directory] -> Env $ pack directory
             _ -> Env B.empty
 
-    BLC.putStrLn $ "Listening on " <> BLC.pack host <> ":" <> BLC.pack port
-    serve (Host host) port $ \(serverSocket, serverAddr) ->
-        do
-            BLC.putStrLn $ "Accepted connection from " <> BLC.pack (show serverAddr) <> "."
-            bsReq <- fromMaybe B.empty <$> recv serverSocket 1024
-            bsRes <- handle env bsReq
-            --print bsReq
-            --print bsRes
-            let isSse = B.empty == getHeader "text/event-stream" (headers' bsRes)
-            if isSse
-                then do
-                    print "SSE START\n\n"
-                    send serverSocket (toBs bsRes)
-                    print (toBs bsRes)
-                    forever $ do
-                        --forkIO $ handle conn store
-                        print "SSE\n\n"
-                        send serverSocket did
-                        send serverSocket ddata
-                else send serverSocket (toBs bsRes)
+    BLC.putStrLn $ "Listening on port: " <> BLC.pack (show port)
+
+    sock <- socket AF_INET Stream defaultProtocol
+    setSocketOption sock ReuseAddr 1
+    bind sock (SockAddrInet port 0)
+    listen sock 5
+    forever $ do
+        (conn, address) <- accept sock
+        forkIO $ eachConn env conn
